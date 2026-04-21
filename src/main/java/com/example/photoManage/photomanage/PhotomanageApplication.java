b2181dff4809c92f1c491df66788f0f338daf96d
kdmer <1909174774@qq.com>
2026-04-22 02:05:45 +0800

    Initial commit

---
+++ src/main/java/com/example/photoManage/photomanage/PhotomanageApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoManage.photomanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotomanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotomanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photoManage/photomanage/PhotomanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoManage.photomanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotomanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
